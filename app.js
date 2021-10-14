e59b5376bcdb9c324d98001e0423810be28c1158
kimjeehey <80799002+kimjeehey@users.noreply.github.com>
2021-10-14 17:00:15 +1100

    JS Events

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
 document.title = "You can change the title from JS";
 
 const title = document.getElementById("title");
+// Query selector has to include CSS format
+const titleTwo = document.querySelector("#title");
 // document.getElementById("title");
 title.innerHTML = "yaa!";
+
+const hello = document.getElementsByClassName("hi");
+const hi = document.getElementsByTagName("h2");
+
+// bring the first element
+const name = document.querySelectorAll("body h2:first-child");
+// bring everything
+const names = document.querySelector(".text");
+
+console.log(hello);
+
+names.innerText = "Click me!";
+
+// find events start with "on" in console
+console.dir(names);
+
+// eventListner
+function handleTitleClick() {
+  console.log("title was clicked");
+  names.innerText = "Yey!";
+}
+
+function handleTitleEnter() {
+  names.style.color = "orange";
+}
+
+function handleTitleLeave() {
+  names.style.color = "green";
+  names.innerText = "Click me";
+}
+
+names.addEventListener("click", handleTitleClick);
+names.addEventListener("mouseenter", handleTitleEnter);
+names.addEventListener("mouseleave", handleTitleLeave);
