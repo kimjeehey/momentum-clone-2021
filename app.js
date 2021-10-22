bf435316d3fabeba583b4903549ba28633267ed3
kimjeehey <80799002+kimjeehey@users.noreply.github.com>
2021-10-22 15:35:51 +1100

    toggle JS

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 const title = document.querySelector(".title");
 
-// function handleTitleClick() {
-//   const currentColor = title.style.color;
-//   let newColor;
-
-//   if (currentColor === "blue") {
-//     newColor = "green";
-//   } else {
-//     newColor = "blue";
-//   }
-//   title.style.color = newColor;
-// } 동일하게 작동하는 코드 아래에.
-
 function handleTitleClick() {
-  const clickedClass = "clicked";
-  // title.className = "active";
-  if (title.classList.contains(clickedClass)) {
-    title.classList.remove(clickedClass);
-  } else {
-    title.classList.add(clickedClass);
-  }
+  title.classList.toggle("clicked");
+  // const clickedClass = "clicked";
+  // if (title.classList.contains(clickedClass)) {
+  //   title.classList.remove(clickedClass);
+  // } else {
+  //   title.classList.add(clickedClass);
+  // }
 }
 
 title.addEventListener("click", handleTitleClick);
