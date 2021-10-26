f39ce07c884b5bb105d837590f4b54a929a701ad
kimjeehey <80799002+kimjeehey@users.noreply.github.com>
2021-10-26 15:16:28 +1100

    preventDefault 2

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = loginForm.querySelector("input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   const username = loginInput.value;
   event.preventDefault();
   console.log(event);
 }
 
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
