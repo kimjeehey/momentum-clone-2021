15f65b02962878bb8a13a68deea111a203044312
kimjeehey <80799002+kimjeehey@users.noreply.github.com>
2021-11-03 13:28:58 +1100

    image added, form style fixed

--- js/background.js
+++ js/background.js
@@ -1,8 +1,12 @@
 // const images = ["01.jpeg", "02.jpeg", "03.jpeg"];
 const images = [
-  "https://images.unsplash.com/photo-1581673001335-f7ba9606c8b2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTU4fHxmaWxtJTIwcGhvdG9ncmFwaHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1597080371188-f699b5639f2d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTR8fGZpbG0lMjBwaG90b2dyYXBoeXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
   "https://images.unsplash.com/photo-1566544054569-951d1512c66c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTgzfHxmaWxtJTIwcGhvdG9ncmFwaHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
   "https://images.unsplash.com/photo-1626212701109-12fd94ddb7f7?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTk5fHxmaWxtJTIwcGhvdG9ncmFwaHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1570081724200-f37eba0ac80c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwxNjA1Njc5NHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1600264094337-4acec0445570?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGZpbG0lMjBwaG90b2dyYXBoeXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1603118258727-c20d28b28bea?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTc3fHxmaWxtJTIwcGhvdG9ncmFwaHl8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1596836530514-3b80da244256?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzIzfHxmaWxtJTIwcGhvdG9ncmFwaHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
