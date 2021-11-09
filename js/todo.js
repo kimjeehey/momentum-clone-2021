59b88c6cafc73a93f9bf79feae3d562bdf47cb47
kimjeehey <80799002+kimjeehey@users.noreply.github.com>
2021-11-09 12:21:26 +1100

    Part 1 - Saving Todos

--- js/todo.js
+++ js/todo.js
@@ -1,10 +1,16 @@
 const todoForm = document.querySelector(".todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector(".todo-list");
+const addButton = document.createElement("button");
+addButton.innerText = "Add";
+todoForm.appendChild(addButton);
 
-function deleteTodo(event) {
-  const li = event.target.parentElement;
-  li.remove();
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveTodos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function paintTodo(newTodo) {
@@ -23,11 +29,29 @@ function paintTodo(newTodo) {
   list.appendChild(button);
 }
 
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
+function handleTodoAdd(event) {
+  event.preventDefault();
+}
+
 function handleTodoSubmit(event) {
   event.preventDefault();
   const newTodo = todoInput.value;
   todoInput.value = "";
+  toDos.push(newTodo);
   paintTodo(newTodo);
+  saveTodos();
 }
 
 todoForm.addEventListener("submit", handleTodoSubmit);
+
+const savedTodos = localStorage.getItem(TODOS_KEY);
+
+if (savedTodos !== null) {
+  const parsedTodos = JSON.parse(savedTodos);
+  parsedTodos.forEach((item) => console.log("turn", item));
+}
