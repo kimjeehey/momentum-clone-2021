bc499a6c324e51374ad524906fd4f7da4ee20fbf
kimjeehey <80799002+kimjeehey@users.noreply.github.com>
2021-11-10 18:30:49 +1100

    Details fixed

--- js/todo.js
+++ js/todo.js
@@ -15,12 +15,12 @@ function saveTodos() {
 
 function paintTodo(newTodo) {
   const list = document.createElement("li");
-  list.className = "todo-list-li";
+  list.className = "todo-list__li";
   const span = document.createElement("span");
   span.innerText = newTodo;
 
   const button = document.createElement("img");
-  button.src = "https://img.icons8.com/ios-glyphs/24/ffffff/multiply.png";
+  button.src = "https://img.icons8.com/ios-glyphs/72/ffffff/multiply.png";
   button.className = "delete-button";
   button.addEventListener("click", deleteTodo);
 
@@ -31,11 +31,8 @@ function paintTodo(newTodo) {
 
 function deleteTodo(event) {
   const li = event.target.parentElement;
-  li.remove();
-}
 
-function handleTodoAdd(event) {
-  event.preventDefault();
+  li.remove();
 }
 
 function handleTodoSubmit(event) {
